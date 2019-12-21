87463787d51a009d58c6543bf29c53385e9f3a2e
Vueler <tim@kitebot.ca>
2019-12-21 18:40:23 -0500

    testin

---
+++ src/routes/index.svelte
@@ -0,0 +1,45 @@
+<style>
+	h1, figure, p {
+		text-align: center;
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+	figure {
+		margin: 0 0 1em 0;
+	}
+
+	img {
+		width: 100%;
+		max-width: 400px;
+		margin: 0 0 1em 0;
+	}
+
+	p {
+		margin: 1em auto;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
+
+<svelte:head>
+	<title>Kitebot</title>
+</svelte:head>
+
+<h1>Kitebot</h1>
+
+<figure>
+	<figcaption>WIP :D</figcaption>
+</figure>
+
+<p><strong>Testing some ideas re:routes</strong></p>
