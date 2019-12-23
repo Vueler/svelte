9d8fae53d87d0aa059a5603655d1df9da8cd2d15
Vueler <tim@kitebot.ca>
2019-12-23 16:19:50 -0500

    double fixed

--- src/routes/euler/[slug].svelte
+++ src/routes/euler/[slug].svelte
@@ -2,11 +2,11 @@
 	export async function preload({ params, query }) {
 		// the `slug` parameter is available because
 		// this file is called [slug].svelte
-		const res = await this.fetch(`blog/${params.slug}.json`);
+		const res = await this.fetch(`euler/${params.slug}.json`);
 		const data = await res.json();
 
 		if (res.status === 200) {
-			return { post: data };
+			return { problem: data };
 		} else {
 			this.error(res.status, data.message);
 		}
